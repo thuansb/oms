936aa40f8e26cc200660b4c5114f5c241710f8bc
DESKTOP-20PI695\thuan <thuan.suba@gmail.com>
2017-10-04 20:41:14 +0700

    styling

--- oms-json-format/src/components/Editor.js
+++ oms-json-format/src/components/Editor.js
@@ -2,11 +2,15 @@
 import React from 'react';
 
 function Displayer({ isValid, cols, handleInputChange, defaultValue }) {
-    const InvalidInputMessage = <span className="App-main-editor--error">(not valid input data)</span>;    
+    const InvalidInputMessage = <span className="App-main-editor--error">(not valid input data)</span>;
     return (
         <div className="App-main-editor">
             <div>Input (editable field) {!isValid && InvalidInputMessage}</div>
-            <textarea rows="35" cols={cols} defaultValue={defaultValue} onChange={(e) => handleInputChange(e.target.value)} />
+            <textarea
+                rows="35"
+                cols={cols}
+                defaultValue={defaultValue}
+                onChange={(e) => handleInputChange(e.target.value)} />
         </div>
     )
 }
